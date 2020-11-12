0c595884ad94d79ec2c9250e2e15f578b8d40f0f
duy.pham <duy.pham@logixtek.com>
2020-11-12 11:16:45 +0700

    Add project files.

---
+++ MoreControlFlowInIf.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeSmell
+{
+    class MoreControlFlowInIf
+    {
+        public string FirstVersion(string mystring)
+        {
+            string[] cars1 = { "Volvo", "BMW", "Ford", "Mazda" };
+            string[] cars2 = { "Volvo", "BMW", "Ford", "Mazda" };
+            string chuoitam = "";
+            for (var j = 0; j < 4; j++)
+            {
+                string mycar1 = cars1[j];
+                string mycar2 = cars2[j];
+
+                if (mycar1 == mycar2 && (mystring == "Program")) // nest more control flow statements 
+                {
+                    chuoitam += " * " + mycar1.ToString();
+                }
+            }
+            return chuoitam;
+        }
+        public string RefactorVersion(string mystring)
+        {
+            string[] cars1 = { "Volvo", "BMW", "Ford", "Mazda" };
+            string[] cars2 = { "Volvo", "BMW2", "Ford2", "Mazda" };
+            string chuoitam = "";
+            for (var j = 0; j < 4; j++)
+            {
+                string mycar1 = cars1[j];
+                string mycar2 = cars2[j];
+
+                if (!(mystring == "Program"))
+                {
+                    continue;
+                }
+                if (!(mycar1 == mycar2))
+                {
+                    continue;
+                }
+                chuoitam += " * " + mycar1.ToString(); 
+            }
+            return chuoitam;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace CodeSmell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myString = "Program";
+            MoreControlFlowInIf refactorif = new MoreControlFlowInIf();
+         //   myString = refactorif.FirstVersion(myString);
+            myString = refactorif.RefactorVersion(myString);
+            Console.WriteLine($"{myString} is Final");
+        }
+    }
+}
